bdb1fe662c2e080660e7b524739b8f66f6257d73
songshengping <songsp@gmail.com>
2026-01-28 10:34:30 +0800

    first commit

---
+++ src/test/java/com/ai/lxy/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.lxy.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
